cc7aae5054eb6a2d9d47ddc1ab40f06ae49bbb73
Doct0rDuc <federicoiannucci2@gmail.com>
2024-11-15 21:17:09 +0100

    created list users function for /users endpoint

--- service/api/get-users.go
+++ service/api/get-users.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"Project_WASAtext/service/api/reqcontext"
+	"encoding/json"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
 )
@@ -9,6 +10,17 @@ import (
 // getContextReply is an example of HTTP endpoint that returns "Hello World!" as a plain text. The signature of this
 // handler accepts a reqcontext.RequestContext (see httpRouterHandler).
 func (rt *_router) getUsers(w http.ResponseWriter, r *http.Request, ps httprouter.Params, ctx reqcontext.RequestContext) {
-	w.Header().Set("content-type", "text/plain")
-	_, _ = w.Write([]byte("Hello World!"))
+	users, err := rt.db.GetUsers()
+	if err != nil {
+		rt.baseLogger.Error("database error: ", err.Error())
+	}
+
+	//Imposto il tipo di contenuto come JSON
+	w.Header().Set("content-type", "application/json")
+
+	//Codifico i dati contenuti in users in formato JSON e li scrivo nella risposta HTTP
+	if err := json.NewEncoder(w).Encode(users); err != nil {
+		http.Error(w, "Errore codifica JSON", http.StatusInternalServerError)
+		rt.baseLogger.Error("Errore nella codifica JSON", err.Error())
+	}
 }
