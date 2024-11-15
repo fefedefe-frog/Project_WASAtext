46f6c8d384c5e8afa69249e5f76295775ad28718
Doct0rDuc <federicoiannucci2@gmail.com>
2024-11-15 21:18:53 +0100

    sketch for other endpoint

--- service/api/api-handler.go
+++ service/api/api-handler.go
@@ -12,21 +12,24 @@ func (rt *_router) Handler() http.Handler {
 	//rt.router.GET("/context", rt.wrap(rt.getContextReply))
 
 	//Login operation
-	rt.router.POST("/session", rt.wrap((rt.postSession)))
+	rt.router.POST("/session", rt.wrap(rt.postSession))
 
 	//User operations
 	rt.router.GET("/users", rt.wrap(rt.getUsers))
-	rt.router.GET("/users/{usr_id}", rt.wrap(rt.getUserInfo))
-	rt.router.PATCH("/users/{usr_id}", rt.wrap(rt.patchChangeUserName))
-	rt.router.GET("/users/{usr_id/propic", rt.wrap(rt.patchChangeUserPhoto))
+	rt.router.GET("/users/:usr_id", rt.wrap(rt.getUserInfo))
+	rt.router.PATCH("/users/:usr_id", rt.wrap(rt.patchChangeUserName))
+	rt.router.GET("/users/:usr_id/propic", rt.wrap(rt.patchChangeUserPhoto))
 
 	//Chat operations
 	rt.router.GET("/chats", rt.wrap(rt.getUserChat))
-	rt.router.DELETE("/chats/{chat_id}", rt.wrap(rt.leaveChat))
-	rt.router.GET("/chats/{chat_id}", rt.wrap(rt.getChatNameAndPhoto))
-	rt.router.GET("/chats/{chat_id}/messages", rt.wrap(rt.getChatMessages))
+	rt.router.DELETE("/chats/:chat_id", rt.wrap(rt.leaveChat))
+	rt.router.GET("/chats/:chat_id", rt.wrap(rt.getChatNameAndPhoto))
+	rt.router.GET("/chats/:chat_id/messages", rt.wrap(rt.getChatMessages))
 
 	//Group operations
+
+	//Message operations
+
 
 	// Special routes
 	rt.router.GET("/liveness", rt.liveness)
--- service/api/get-chat-messages.go
+++ service/api/get-chat-messages.go
@@ -1,1 +1,12 @@
 package api
+
+import (
+	"Project_WASAtext/service/api/reqcontext"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+func (rt *_router) getChatMessages(writer http.ResponseWriter, request *http.Request, params httprouter.Params, context reqcontext.RequestContext) {
+	//TODO implement me
+	panic("implement me")
+}
