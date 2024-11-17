9999ec0d3a8c815fd72c1ea87ea5abf1644fee10
Doct0rDuc <federicoiannucci2@gmail.com>
2024-11-17 03:41:56 +0100

    cleared unused code

--- service/api/api-handler.go
+++ service/api/api-handler.go
@@ -7,18 +7,14 @@ import (
 // Handler returns an instance of httprouter.Router that handle APIs registered here
 func (rt *_router) Handler() http.Handler {
 
-	// Register routes
-	//rt.router.GET("/", rt.getHelloWorld)
-	//rt.router.GET("/context", rt.wrap(rt.getContextReply))
-
 	//Login operation
 	rt.router.POST("/session", rt.wrap(rt.postSession))										//fatto
 
 	//User operations
 	rt.router.GET("/users", rt.wrap(rt.BearerAuth(rt.getUsers)))								//fatto
 	rt.router.GET("/users/:usr_id", rt.wrap(rt.BearerAuth(rt.getUserInfo)))					//fatto
-	rt.router.PATCH("/users/:usr_id", rt.wrap(rt.BearerAuth(rt.patchChangeUserName)))
-	rt.router.GET("/users/:usr_id/propic", rt.wrap(rt.BearerAuth(rt.patchChangeUserPhoto)))	//TODO
+	rt.router.PATCH("/users/:usr_id", rt.wrap(rt.BearerAuth(rt.patchChangeUserName)))			//fatto
+	rt.router.PATCH("/users/:usr_id/propic", rt.wrap(rt.BearerAuth(rt.patchChangeUserPhoto)))	//fatto
 
 	//Chat operations
 	rt.router.GET("/chats", rt.wrap(rt.BearerAuth(rt.getUserChat)))							//TODO
