9e97cb4770c56088e51179842c8e2fa0f3ad26a9
Doct0rDuc <federicoiannucci2@gmail.com>
2024-11-18 02:49:53 +0100

    made sketch functions, missing other

---
+++ service/api/api-chatoperation.go
@@ -0,0 +1,29 @@
+package api
+
+import (
+	"Project_WASAtext/service/api/reqcontext"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+func (rt *_router) getUserChats(writer http.ResponseWriter, request *http.Request, params httprouter.Params, context reqcontext.RequestContext) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (rt *_router) getChatMessages(writer http.ResponseWriter, request *http.Request, params httprouter.Params, context reqcontext.RequestContext) {
+	//TODO implement me
+	panic("implement me")
+}
+
+
+func (rt *_router) getChatInfo(writer http.ResponseWriter, request *http.Request, params httprouter.Params, context reqcontext.RequestContext) {
+	//TODO implement me
+	panic("implement me")
+}
+
+
+func (rt *_router) leaveChat(writer http.ResponseWriter, request *http.Request, params httprouter.Params, context reqcontext.RequestContext) {
+	//TODO implement me
+	panic("implement me")
+}
