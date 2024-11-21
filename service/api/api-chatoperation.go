0f663d95568d33bdc0eed97616deee268bf139a1
Doct0rDuc <federicoiannucci2@gmail.com>
2024-11-21 03:19:04 +0100

    added token parameter to functions

--- service/api/api-chatoperation.go
+++ service/api/api-chatoperation.go
@@ -6,24 +6,24 @@ import (
 	"net/http"
 )
 
-func (rt *_router) getUserChats(writer http.ResponseWriter, request *http.Request, params httprouter.Params, context reqcontext.RequestContext) {
+func (rt *_router) getUserChats(writer http.ResponseWriter, request *http.Request, params httprouter.Params, context reqcontext.RequestContext, token string) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (rt *_router) getChatMessages(writer http.ResponseWriter, request *http.Request, params httprouter.Params, context reqcontext.RequestContext) {
-	//TODO implement me
-	panic("implement me")
-}
-
-
-func (rt *_router) getChatInfo(writer http.ResponseWriter, request *http.Request, params httprouter.Params, context reqcontext.RequestContext) {
+func (rt *_router) getChatMessages(writer http.ResponseWriter, request *http.Request, params httprouter.Params, context reqcontext.RequestContext, token string) {
 	//TODO implement me
 	panic("implement me")
 }
 
 
-func (rt *_router) leaveChat(writer http.ResponseWriter, request *http.Request, params httprouter.Params, context reqcontext.RequestContext) {
+func (rt *_router) getChatInfo(writer http.ResponseWriter, request *http.Request, params httprouter.Params, context reqcontext.RequestContext, token string) {
+	//TODO implement me
+	panic("implement me")
+}
+
+
+func (rt *_router) leaveChat(writer http.ResponseWriter, request *http.Request, params httprouter.Params, context reqcontext.RequestContext, token string) {
 	//TODO implement me
 	panic("implement me")
 }
