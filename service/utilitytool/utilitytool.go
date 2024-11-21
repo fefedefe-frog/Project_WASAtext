d7a4830d5dd2bc054213599aee141a98bf468327
Doct0rDuc <federicoiannucci2@gmail.com>
2024-11-21 03:11:54 +0100

    added error variable for comparison in other file

--- service/utilitytool/utilitytool.go
+++ service/utilitytool/utilitytool.go
@@ -6,34 +6,42 @@ import (
 	"unicode/utf8"
 )
 
-func UserNameIsValid (userName string) (bool, error){
+var (
+	ErrInvalidRegex = errors.New("INVALID REGEX")
+	ErrNameShort    = errors.New("NAME SHORT")
+	ErrNameLong     = errors.New("NAME LONG")
+	ErrIdShort		= errors.New("ID SHORT")
+	ErrIdLong		= errors.New("ID LONG")
+)
+
+func UserNameIsValid (userName string) error{
 
 	//Controllo che rispetti i regex richiesti
 	if !regexp.MustCompile(`^\S.*\S$`).MatchString(userName){
-		return false, errors.New("INVALID REGEX")
+		return ErrInvalidRegex
 	}
 	if utf8.RuneCountInString(userName)<3{	//Deve essere lungo almeno 3 caratteri
-		return false, errors.New("NAME SHORT")
+		return ErrNameShort
 	}
 	if utf8.RuneCountInString(userName)>16{	//Deve essere lungo massimo 16 caratteri
-		return false, errors.New("NAME LONG")
+		return ErrNameLong
 	}
 
-	return true, nil
+	return nil
 }
 
-func UsrIdIsValid (usrId string) (bool, error){
+func UsrIdIsValid (usrId string) error{
 
 	//Controllo che rispetti i regex richiesti
 	if !regexp.MustCompile(`\s+`).MatchString(usrId){
-		return false, errors.New("INVALID ID REGEX")
+		return ErrInvalidRegex
 	}
 	if utf8.RuneCountInString(usrId)<3{	//Deve essere lungo almeno 3 caratteri
-		return false, errors.New("ID SHORT")
+		return ErrIdShort
 	}
 	if utf8.RuneCountInString(usrId)>32{	//Deve essere lungo massimo 32 caratteri
-		return false, errors.New("ID LONG")
+		return ErrIdLong
 	}
 
-	return true, nil
+	return nil
 }
