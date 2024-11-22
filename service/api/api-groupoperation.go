9d0a8925f14114118395d569fd10261dd4b324e6
Doct0rDuc <federicoiannucci2@gmail.com>
2024-11-22 04:01:42 +0100

    added group chat api folder

---
+++ service/api/api-groupoperation.go
@@ -0,0 +1,12 @@
+package api
+
+import (
+	"Project_WASAtext/service/api/reqcontext"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+func (rt *_router) leaveGroup(writer http.ResponseWriter, request *http.Request, params httprouter.Params, context reqcontext.RequestContext, token string) {
+	//TODO implement me
+	panic("implement me")
+}
