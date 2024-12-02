5cae9b73506030a805be5fd94cde31663ed302b7
Doct0rDuc <federicoiannucci2@gmail.com>
2024-12-02 02:49:52 +0100

    added missing api router handlers

--- service/api/api-handler.go
+++ service/api/api-handler.go
@@ -17,19 +17,27 @@ func (rt *_router) Handler() http.Handler {
 	rt.router.PATCH("/users/:usr_id/propic", rt.wrap(rt.BearerAuth(rt.patchChangeUserPhoto)))	//fatto
 
 	//Chat operations
-	rt.router.GET("/chats", rt.wrap(rt.BearerAuth(rt.getUserChats)))							//TODO
-	rt.router.GET("/chats/:chat_id/messages", rt.wrap(rt.BearerAuth(rt.getChatMessages)))		//TODO
-	rt.router.GET("/chats/:chat_id", rt.wrap(rt.BearerAuth(rt.getChatInfo)))					//TODO
+	rt.router.POST("/chats", rt.wrap(rt.BearerAuth(rt.startNewChat)))							//TODO
+	rt.router.GET("/chats", rt.wrap(rt.BearerAuth(rt.getUserChats)))							//fatto
+	rt.router.GET("/chats/:chat_id", rt.wrap(rt.BearerAuth(rt.getChatInfo)))					//fatto
+	rt.router.GET("/chats/:chat_id/messages", rt.wrap(rt.BearerAuth(rt.getChatMessages)))		//fatto
 
 	//Group operations
-	//TODO
-	rt.router.DELETE("/chats/:chat_id", rt.wrap(rt.BearerAuth(rt.leaveGroup)))					//TODO
+	rt.router.DELETE("/chats/:chat_id/users", rt.wrap(rt.BearerAuth(rt.leaveGroup)))			//fatto
+	rt.router.PATCH("/chats/:chat_id", rt.wrap(rt.BearerAuth(rt.changeGroupName)))				//fatto
+	rt.router.PATCH("/chats/:chat_id/propic", rt.wrap(rt.BearerAuth(rt.changeGroupPhoto)))		//fatto
+	rt.router.POST("/chats/:chat_id/users", rt.wrap(rt.BearerAuth(rt.postAddUserToGroup)))		//fatto
 
 	//Message operations
-	//TODO
+	rt.router.POST("/chats/:chat_id/messages", rt.wrap(rt.BearerAuth(rt.sendMessage)))			//TODO
+	rt.router.POST("/chats/:chat_id/messages", rt.wrap(rt.BearerAuth(rt.deleteMessage)))		//TODO
+	rt.router.POST("/chats/:chat_id/messages", rt.wrap(rt.BearerAuth(rt.forwardMessage)))		//TODO
+	rt.router.POST("/chats/:chat_id/messages", rt.wrap(rt.BearerAuth(rt.getMessageComments)))	//TODO
+	rt.router.POST("/chats/:chat_id/messages", rt.wrap(rt.BearerAuth(rt.commentMessage)))		//TODO
+	rt.router.POST("/chats/:chat_id/messages", rt.wrap(rt.BearerAuth(rt.uncommentMessage)))	//TODO
 
 	// Special routes
-	rt.router.GET("/liveness", rt.liveness)
+	//rt.router.GET("/liveness", rt.liveness)
 
 	return rt.router
 }
