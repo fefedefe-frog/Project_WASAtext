f8ca189c0820de7ab6e30e1b33a5fe814dc3ad11
Doct0rDuc <federicoiannucci2@gmail.com>
2024-12-03 23:37:56 +0100

    started working on messages api

--- service/api/api-handler.go
+++ service/api/api-handler.go
@@ -8,33 +8,33 @@ import (
 func (rt *_router) Handler() http.Handler {
 
 	//Login operation
-	rt.router.POST("/session", rt.wrap(rt.postSession))										//fatto
+	rt.router.POST("/session", rt.wrap(rt.postSession)) //fatto
 
 	//User operations
-	rt.router.GET("/users", rt.wrap(rt.BearerAuth(rt.getUsers)))								//fatto
-	rt.router.GET("/users/:usr_id", rt.wrap(rt.BearerAuth(rt.getUserInfo)))					//fatto
-	rt.router.PATCH("/users/:usr_id", rt.wrap(rt.BearerAuth(rt.patchChangeUserName)))			//fatto
-	rt.router.PATCH("/users/:usr_id/propic", rt.wrap(rt.BearerAuth(rt.patchChangeUserPhoto)))	//fatto
+	rt.router.GET("/users", rt.wrap(rt.BearerAuth(rt.getUsers)))                              //fatto
+	rt.router.GET("/users/:usr_id", rt.wrap(rt.BearerAuth(rt.getUserInfo)))                   //fatto
+	rt.router.PATCH("/users/:usr_id", rt.wrap(rt.BearerAuth(rt.patchChangeUserName)))         //fatto
+	rt.router.PATCH("/users/:usr_id/propic", rt.wrap(rt.BearerAuth(rt.patchChangeUserPhoto))) //fatto
 
 	//Chat operations
-	rt.router.POST("/chats", rt.wrap(rt.BearerAuth(rt.startNewChat)))							//TODO
-	rt.router.GET("/chats", rt.wrap(rt.BearerAuth(rt.getUserChats)))							//fatto
-	rt.router.GET("/chats/:chat_id", rt.wrap(rt.BearerAuth(rt.getChatInfo)))					//fatto
-	rt.router.GET("/chats/:chat_id/messages", rt.wrap(rt.BearerAuth(rt.getChatMessages)))		//fatto
+	rt.router.POST("/chats", rt.wrap(rt.BearerAuth(rt.startNewChat)))                     //TODO
+	rt.router.GET("/chats", rt.wrap(rt.BearerAuth(rt.getUserChats)))                      //fatto
+	rt.router.GET("/chats/:chat_id", rt.wrap(rt.BearerAuth(rt.getChatInfo)))              //fatto
+	rt.router.GET("/chats/:chat_id/messages", rt.wrap(rt.BearerAuth(rt.getChatMessages))) //fatto
 
 	//Group operations
-	rt.router.DELETE("/chats/:chat_id/users", rt.wrap(rt.BearerAuth(rt.leaveGroup)))			//fatto
-	rt.router.PATCH("/chats/:chat_id", rt.wrap(rt.BearerAuth(rt.changeGroupName)))				//fatto
-	rt.router.PATCH("/chats/:chat_id/propic", rt.wrap(rt.BearerAuth(rt.changeGroupPhoto)))		//fatto
-	rt.router.POST("/chats/:chat_id/users", rt.wrap(rt.BearerAuth(rt.postAddUserToGroup)))		//fatto
+	rt.router.DELETE("/chats/:chat_id/users", rt.wrap(rt.BearerAuth(rt.leaveGroup)))       //fatto
+	rt.router.PATCH("/chats/:chat_id", rt.wrap(rt.BearerAuth(rt.changeGroupName)))         //fatto
+	rt.router.PATCH("/chats/:chat_id/propic", rt.wrap(rt.BearerAuth(rt.changeGroupPhoto))) //fatto
+	rt.router.POST("/chats/:chat_id/users", rt.wrap(rt.BearerAuth(rt.postAddUserToGroup))) //fatto
 
 	//Message operations
-	rt.router.POST("/chats/:chat_id/messages", rt.wrap(rt.BearerAuth(rt.sendMessage)))			//TODO
-	rt.router.POST("/chats/:chat_id/messages", rt.wrap(rt.BearerAuth(rt.deleteMessage)))		//TODO
-	rt.router.POST("/chats/:chat_id/messages", rt.wrap(rt.BearerAuth(rt.forwardMessage)))		//TODO
-	rt.router.POST("/chats/:chat_id/messages", rt.wrap(rt.BearerAuth(rt.getMessageComments)))	//TODO
-	rt.router.POST("/chats/:chat_id/messages", rt.wrap(rt.BearerAuth(rt.commentMessage)))		//TODO
-	rt.router.POST("/chats/:chat_id/messages", rt.wrap(rt.BearerAuth(rt.uncommentMessage)))	//TODO
+	rt.router.POST("/chats/:chat_id/messages", rt.wrap(rt.BearerAuth(rt.sendMessage)))        //fatto
+	rt.router.POST("/chats/:chat_id/messages", rt.wrap(rt.BearerAuth(rt.deleteMessage)))      //fatto
+	rt.router.POST("/chats/:chat_id/messages", rt.wrap(rt.BearerAuth(rt.forwardMessage)))     //TODO
+	rt.router.POST("/chats/:chat_id/messages", rt.wrap(rt.BearerAuth(rt.getMessageComments))) //TODO
+	rt.router.POST("/chats/:chat_id/messages", rt.wrap(rt.BearerAuth(rt.commentMessage)))     //TODO
+	rt.router.POST("/chats/:chat_id/messages", rt.wrap(rt.BearerAuth(rt.uncommentMessage)))   //TODO
 
 	// Special routes
 	//rt.router.GET("/liveness", rt.liveness)
--- service/api/api-messageoperation.go
+++ service/api/api-messageoperation.go
@@ -7,6 +7,7 @@ import (
 	"encoding/json"
 	"errors"
 	"github.com/julienschmidt/httprouter"
+	"github.com/sirupsen/logrus"
 	"net/http"
 	"strconv"
 )
@@ -15,18 +16,19 @@ func (rt *_router) sendMessage(writer http.ResponseWriter, request *http.Request
 	context.Logger.Info("POST request to endpoint /chats/{chat_id}")
 
 	requestJson := struct {
-		ContentType 	string `json:"contentType"`
-		Content     	string `json:"content"`
+		ContentType string `json:"contentType"`
+		Content     string `json:"content"`
 	}{}
 
-	//Controllo che l'utente faccia effettivamente parte del gruppo
+	//Recupero il chat id dai parametri dell'endpoint
 	chatId, err := strconv.Atoi(params.ByName("chat_id"))
 	if err != nil {
 		rt.baseLogger.WithError(err).Error("invalid chat id")
 		http.Error(writer, "invalid chat_id parameter", http.StatusBadRequest)
 	}
-	if isParticipant, err := rt.db.CheckIfUserIsParticipant(chatId, token); !isParticipant{
-		if errors.Is(err, sql.ErrNoRows){
+	//Controllo che l'utente faccia effettivamente parte del gruppo
+	if isParticipant, err := rt.db.CheckIfUserIsParticipant(chatId, token); !isParticipant {
+		if errors.Is(err, sql.ErrNoRows) {
 			context.Logger.Warnf("user <%s> tried to send a message to the chat <%d> which he isn't a member of", token, chatId)
 			http.Error(writer, "Forbidden - can't send a message of a chat which aren't member of", http.StatusForbidden)
 			return
@@ -46,11 +48,11 @@ func (rt *_router) sendMessage(writer http.ResponseWriter, request *http.Request
 	context.Logger.Infof("user <%s> want to send a message to chat <%d>", token, chatId)
 	//Aggiungo il messaggio al db
 	var newMessage database.Message
-	newMessage.SenderId= token
+	newMessage.SenderId = token
 	newMessage.ContentType = requestJson.ContentType
 	newMessage.Content = requestJson.Content
-	newMessage.DeliveryStatus= "sent"
-	newMessage.IsForwarded= false
+	newMessage.DeliveryStatus = "sent"
+	newMessage.IsForwarded = false
 
 	if err := rt.db.InsertMessage(newMessage, chatId); err != nil {
 		rt.baseLogger.WithError(err).Error("Error while inserting new message in the db")
@@ -64,6 +66,56 @@ func (rt *_router) sendMessage(writer http.ResponseWriter, request *http.Request
 }
 
 func (rt *_router) deleteMessage(writer http.ResponseWriter, request *http.Request, params httprouter.Params, context reqcontext.RequestContext, token string) {
+	context.Logger.Info("DELETE request to endpoint /chats/{chat_id}/messages/{msg_id}")
+
+	//Recupero l'id della chat e del messaggio dai paramentri dell'endpoint
+	chatId, err := strconv.Atoi(params.ByName("chat_id"))
+	if err != nil {
+		rt.baseLogger.WithError(err).Error("invalid chat id")
+		http.Error(writer, "invalid chat_id parameter", http.StatusBadRequest)
+	}
+	var msgId int
+	msgId, err = strconv.Atoi(params.ByName("msg_id"))
+	if err != nil {
+		rt.baseLogger.WithError(err).Error("invalid message id")
+		http.Error(writer, "invalid msg_id parameter", http.StatusBadRequest)
+	}
+
+	//Controllo che il messaggio esista, e sia stato mandato dall'utente che lo vuole eliminare
+	var senderId string
+	senderId, err = rt.db.GetSenderIdByMsgId(msgId)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			rt.baseLogger.WithError(err).Warn("Message not found in the database")
+			http.Error(writer, "Not Found - Message not found", http.StatusNotFound)
+			return
+		}
+		rt.baseLogger.WithError(err).Error("Error while checking the sender id of a message")
+		http.Error(writer, "Internal Server Error - Unable to delete the message", http.StatusInternalServerError)
+		return
+	}
+
+	if senderId != token {
+		rt.baseLogger.WithField("user", token).Warnf("Error while checking the sender id of a message")
+		http.Error(writer, "Forbidden - You can't delete the message of another user", http.StatusForbidden)
+		return
+	}
+
+	rt.baseLogger.WithField("msgId", msgId).Debug("tryong to remove the message")
+	err = rt.db.RemoveMessage(msgId, chatId)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			rt.baseLogger.WithError(err).WithFields(logrus.Fields{"msgId": msgId, "chatId": chatId}).Warn("Unable to find and remove a message with that msgId and chatId the")
+			http.Error(writer, "Not Found - Unable to find the message by the msgId and chatId gived", http.StatusNotFound)
+			return
+		}
+		rt.baseLogger.WithError(err).WithFields(logrus.Fields{"msgId": msgId, "chatId": chatId}).Warn("Unable to find and remove a message with that msgId and chatId the")
+		http.Error(writer, "Internal Server Error - Unable to delete the message", http.StatusInternalServerError)
+		return
+	}
+
+	rt.baseLogger.WithField("msgId", msgId).Info("message removed successfully")
+	return
 }
 
 func (rt *_router) forwardMessage(writer http.ResponseWriter, request *http.Request, params httprouter.Params, context reqcontext.RequestContext, token string) {
