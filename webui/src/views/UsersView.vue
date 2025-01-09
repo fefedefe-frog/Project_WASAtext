b3c692f661cbf49dd46a4bccc2de3905eb6d369d
Doct0rDuc <federicoiannucci2@gmail.com>
2025-01-09 06:01:40 +0100

    started working on users list

---
+++ webui/src/views/UsersView.vue
@@ -0,0 +1,111 @@
+<script>
+
+export default {
+  data: function () {
+    return {
+      token: localStorage.getItem('authToken'),
+      errormsg: null,
+      loading: false,
+      users: [],
+    }
+  },
+  methods: {
+    async refresh() {
+      this.loading = true
+      this.errormsg = null
+
+      try {
+        let response = await this.$axios.get("/users", {headers: {Authorization: `${this.token}`}});
+        console.log(response.data);
+        this.users = []
+        if (response.data){
+          response.data["users"].forEach(user => {
+            this.users.push(user)
+          })
+        }
+      } catch (e) {
+        this.errormsg = e.toString();
+      } finally {
+        this.loading = false;
+      }
+    },
+    showOverlay() {
+      this.isOverlayVisible = true;
+      console.log('Overlay mostrato:', this.isOverlayVisible);
+    },
+    hideOverlay() {
+      this.isOverlayVisible = false;
+      console.log('Overlay nascosto:', this.isOverlayVisible);
+    },
+
+  },
+  mounted() {
+    this.refresh()
+  }
+}
+</script>
+
+<template>
+  <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="h2">Users</h1>
+    <div class="btn-toolbar mb-2 mb-md-0">
+      <div class="btn-group me-2">
+        <button type="button" class="btn btn-sm btn-outline-secondary" @click="refresh">
+          Refresh
+        </button>
+        <div class="btn-group me-2">
+          <button type="button" class="btn btn-sm btn-outline-primary" @click="showOverlay">
+            New
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    <div id="users_list_container">
+      <LoadingSpinner v-if="loading" loading="{{ loading }}" loadingText="Caricamento chat"/><LoadingSpinner/>
+    </div>
+  </div>
+  <ErrorMsg v-if="errormsg" :msg="errormsg"></ErrorMsg>
+
+</template>
+
+<style>
+#users_list_container{
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  border: #0c4128 4px solid;
+}
+
+/* Contenitore principale */
+.container {
+  position: relative;
+  width: 100%;
+  height: auto;
+  padding: 20px;
+  border: 4px solid darkorchid;
+}
+
+.overlay {
+  position: fixed; /* Cambia a "fixed" per far sì che stia sopra l'intera finestra */
+  top: 50%; /* Posizione centrata verticalmente */
+  left: 50%; /* Posizione centrata orizzontalmente */
+  transform: translate(-50%, -50%); /* Centra esattamente l'overlay */
+  background-color: rgba(0, 0, 0, 0.7); /* Sfondo semi-trasparente */
+  color: white;
+  padding: 20px;
+  border-radius: 10px;
+  border: #0a58ca 6px solid;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
+  z-index: 100; /* Assicurati che l'overlay stia sopra l'altro contenuto */
+}
+
+.modal {
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 10px;
+  text-align: center;
+  color: #333;
+}
+
+</style>
