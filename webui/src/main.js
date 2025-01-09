f885b148b3cd9f54e80ff3150ac5717364717e21
Doct0rDuc <federicoiannucci2@gmail.com>
2025-01-09 05:59:27 +0100

    imported user and chat banners

--- webui/src/main.js
+++ webui/src/main.js
@@ -6,6 +6,7 @@ import axios from './services/axios.js';
 import ErrorMsg from './components/ErrorMsg.vue'
 import LoadingSpinner from './components/LoadingSpinner.vue'
 import ChatBanner from './components/ChatBanner.vue'
+import UserBanner from './components/UserBanner.vue'
 
 import './assets/dashboard.css'
 import './assets/main.css'
@@ -16,6 +17,7 @@ app.config.globalProperties.$axios = axios;
 app.component("ErrorMsg", ErrorMsg);
 app.component("LoadingSpinner", LoadingSpinner);
 app.component("ChatBanner", ChatBanner);
+app.component("UserBanner", UserBanner);
 
 app.use(router)
 app.mount('#app')
