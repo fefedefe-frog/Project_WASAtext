292f676f1440bbdd62905b9218c9d8fab99bf667
Doct0rDuc <federicoiannucci2@gmail.com>
2025-01-15 02:12:36 +0100

    testing css style options

--- webui/src/App.vue
+++ webui/src/App.vue
@@ -66,11 +66,18 @@ export default {
 			</nav>
 
 			<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-				<RouterView />
+				<RouterView/>
 			</main>
 		</div>
 	</div>
 </template>
 
 <style>
+
+main {
+  overflow: hidden;
+  width: 100%;
+  height: 90vh;
+}
+
 </style>
