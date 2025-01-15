7d16abe3bbd284374249c0577392a3c40e72bffd
Doct0rDuc <federicoiannucci2@gmail.com>
2025-01-15 01:43:25 +0100

    added router path for new functions

--- service/api/api-handler.go
+++ service/api/api-handler.go
@@ -17,24 +17,26 @@ func (rt *_router) Handler() http.Handler {
 	rt.router.PUT("/profile/propic", rt.wrap(rt.BearerAuth(rt.putChangeUserPhoto))) 	// fatto
 
 	// Chat operations
-	rt.router.POST("/chats", rt.wrap(rt.BearerAuth(rt.startNewChat)))                            // fatto
-	rt.router.GET("/chats", rt.wrap(rt.BearerAuth(rt.getUserChats)))                             // fatto
-	rt.router.GET("/chats/:chat_id", rt.wrap(rt.BearerAuth(rt.getChatInfo)))                     // fatto
-	rt.router.PUT("/chats/:chat_id/messages", rt.wrap(rt.BearerAuth(rt.putRetriveChatMessages))) // fatto
+	rt.router.POST("/chats", rt.wrap(rt.BearerAuth(rt.startNewChat)))								// fatto
+	rt.router.GET("/chats", rt.wrap(rt.BearerAuth(rt.getUserChats)))								// fatto
+	rt.router.GET("/chats/:chat_id", rt.wrap(rt.BearerAuth(rt.getChatInfo)))						// fatto
+	rt.router.PUT("/chats/:chat_id/messages", rt.wrap(rt.BearerAuth(rt.putRetriveChatMessages)))	// fatto
+	rt.router.GET("/chats/:chat_id/users", rt.wrap(rt.BearerAuth(rt.getChatParticipants)))			// fatto
 
 	// Group operations
 	rt.router.DELETE("/chats/:chat_id/users", rt.wrap(rt.BearerAuth(rt.leaveGroup)))       // fatto
 	rt.router.PUT("/chats/:chat_id", rt.wrap(rt.BearerAuth(rt.changeGroupName)))         	// fatto
-	rt.router.PUT("/chats/:chat_id/propic", rt.wrap(rt.BearerAuth(rt.changeGroupPhoto))) 	// fatto
-	rt.router.POST("/chats/:chat_id/users", rt.wrap(rt.BearerAuth(rt.addUserToGroup)))     // fatto
+	rt.router.PUT("/chats/:chat_id/propic", rt.wrap(rt.BearerAuth(rt.changeGroupPhoto)))	// fatto
+	rt.router.POST("/chats/:chat_id/users", rt.wrap(rt.BearerAuth(rt.addUserToGroup)))		// fatto
 
 	// Message operations
-	rt.router.POST("/chats/:chat_id/messages", rt.wrap(rt.BearerAuth(rt.sendMessage)))                                       // fatto
-	rt.router.DELETE("/chats/:chat_id/messages", rt.wrap(rt.BearerAuth(rt.deleteMessage)))                                   // fatto
-	rt.router.POST("/chats/:chat_id/messages/:msg_id", rt.wrap(rt.BearerAuth(rt.forwardMessage)))                            // fatto
-	rt.router.GET("/chats/:chat_id/messages/:msg_id/comments", rt.wrap(rt.BearerAuth(rt.getMessageComments)))                // fatto
-	rt.router.POST("/chats/:chat_id/messages/:msg_id/comments", rt.wrap(rt.BearerAuth(rt.commentMessage)))                   // fatto
-	rt.router.DELETE("/chats/:chat_id/messages/:msg_id/comments/:commenter_id", rt.wrap(rt.BearerAuth(rt.uncommentMessage))) // fatto
+	rt.router.POST("/chats/:chat_id/messages", rt.wrap(rt.BearerAuth(rt.sendMessage)))											// fatto
+	rt.router.DELETE("/chats/:chat_id/messages", rt.wrap(rt.BearerAuth(rt.deleteMessage)))										// fatto
+	rt.router.POST("/chats/:chat_id/messages/:msg_id", rt.wrap(rt.BearerAuth(rt.forwardMessage)))								// fatto
+	rt.router.PUT("/chats/:chat_id/messages/:msg_id", rt.wrap(rt.BearerAuth(rt.putUpdateMessageStatus)))						// fatto
+	rt.router.GET("/chats/:chat_id/messages/:msg_id/comments", rt.wrap(rt.BearerAuth(rt.getMessageComments)))					// fatto
+	rt.router.POST("/chats/:chat_id/messages/:msg_id/comments", rt.wrap(rt.BearerAuth(rt.commentMessage)))						// fatto
+	rt.router.DELETE("/chats/:chat_id/messages/:msg_id/comments/:commenter_id", rt.wrap(rt.BearerAuth(rt.uncommentMessage)))	// fatto
 
 	return rt.router
 }
