e14a515287fb49a73fa6299012c1cd646078b7f9
Doct0rDuc <federicoiannucci2@gmail.com>
2025-03-05 01:29:47 +0100

    renamed some fuction to match the operationId defined in the api.yaml

--- service/api/api-handler.go
+++ service/api/api-handler.go
@@ -13,21 +13,21 @@ func (rt *_router) Handler() http.Handler {
 	// User operations
 	rt.router.GET("/users", rt.wrap(rt.BearerAuth(rt.getUsers)))						// fatto
 	rt.router.GET("/users/:usr_id", rt.wrap(rt.BearerAuth(rt.getUserInfo)))            // fatto
-	rt.router.PUT("/profile", rt.wrap(rt.BearerAuth(rt.putChangeUserName)))         	// fatto
-	rt.router.PUT("/profile/propic", rt.wrap(rt.BearerAuth(rt.putChangeUserPhoto))) 	// fatto
+	rt.router.PUT("/profile", rt.wrap(rt.BearerAuth(rt.setMyUserName)))         	// fatto
+	rt.router.PUT("/profile/propic", rt.wrap(rt.BearerAuth(rt.setMyPhoto))) 	// fatto
 
 	// Chat operations
 	rt.router.POST("/chats", rt.wrap(rt.BearerAuth(rt.startNewChat)))								// fatto
-	rt.router.GET("/chats", rt.wrap(rt.BearerAuth(rt.getUserChats)))								// fatto
-	rt.router.GET("/chats/:chat_id", rt.wrap(rt.BearerAuth(rt.getChatInfo)))						// fatto
+	rt.router.GET("/chats", rt.wrap(rt.BearerAuth(rt.getMyConversations)))								// fatto
+	rt.router.GET("/chats/:chat_id", rt.wrap(rt.BearerAuth(rt.getConversation)))						// fatto
 	rt.router.PUT("/chats/:chat_id/messages", rt.wrap(rt.BearerAuth(rt.putRetriveChatMessages)))	// fatto
 	rt.router.GET("/chats/:chat_id/users", rt.wrap(rt.BearerAuth(rt.getChatParticipants)))			// fatto
 
 	// Group operations
 	rt.router.DELETE("/chats/:chat_id/users", rt.wrap(rt.BearerAuth(rt.leaveGroup)))       // fatto
-	rt.router.PUT("/chats/:chat_id", rt.wrap(rt.BearerAuth(rt.changeGroupName)))         	// fatto
-	rt.router.PUT("/chats/:chat_id/propic", rt.wrap(rt.BearerAuth(rt.changeGroupPhoto)))	// fatto
-	rt.router.POST("/chats/:chat_id/users", rt.wrap(rt.BearerAuth(rt.addUserToGroup)))		// fatto
+	rt.router.PUT("/chats/:chat_id", rt.wrap(rt.BearerAuth(rt.setGroupName)))         	// fatto
+	rt.router.PUT("/chats/:chat_id/propic", rt.wrap(rt.BearerAuth(rt.setGroupPhoto)))	// fatto
+	rt.router.POST("/chats/:chat_id/users", rt.wrap(rt.BearerAuth(rt.addToGroup)))		// fatto
 
 	// Message operations
 	rt.router.POST("/chats/:chat_id/messages", rt.wrap(rt.BearerAuth(rt.sendMessage)))											// fatto
