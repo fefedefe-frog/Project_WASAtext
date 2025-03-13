9e2ca299af3a4e53792b69eadac896bca08dc56d
Doct0rDuc <federicoiannucci2@gmail.com>
2025-03-13 14:52:01 +0100

    added the message for no chats, adjusted the loading bar

--- webui/src/views/ChatsView.vue
+++ webui/src/views/ChatsView.vue
@@ -7,25 +7,46 @@ export default {
       loading: false,
       chats: [],
       isOverlayVisible: false,
+      showMessage: false,
+      noChats: false,
     }
   },
   methods: {
     async refresh() {
-      this.loading = true
-      this.errormsg = null
+      this.loading= true;
+      this.showMessage= true;
+
+      this.noChats= false;
+      this.errormsg= null;
+
+      this.chats= []
+
+      const delay = new Promise(resolve => setTimeout(resolve, 500));
+      const fetchChats = this.$axios.get("/chats", {headers: {Authorization: `${this.token}`}});
 
       try {
-        let response = await this.$axios.get("/chats", {headers: {Authorization: `${this.token}`}});
-        this.chats = []
-        if (response.data) {
+        let response= await Promise.all([fetchChats, delay]).then(results => results[0]);
+
+        if (response.data){
+          this.showMessage= false;
           response.data["chats"].forEach(chat => {
             this.chats.push(chat)
           })
         }
       } catch (e) {
-        this.errormsg = e.toString();
+        if (e.response) {
+          if (e.response.status === 404) {
+            // In questo caso, se ho l'errore 404 significa solo che l'utente non ha chat, e non è un problema
+            this.noChats= true;
+
+          }else{
+            this.errormsg= e.toString();
+          }
+        }else{
+          this.errormsg= e.toString();
+        }
       } finally {
-        this.loading = false;
+        this.loading= false
       }
     }
   },
@@ -49,25 +70,41 @@ export default {
     </div>
 
     <!-- Div che contiene la lista di chat -->
-    <div id="chat_list_container">
+    <div :class="showMessage ? 'chat-list-show-message' : 'chat-list-container'">
+      <div v-if="showMessage && noChats" class="nochat">
+        <p class="visually-visible centered">Non hai nessuna chat :(</p>
+      </div>
+
+      <LoadingSpinner v-if="loading" :loading="loading" loadingText="Caricamento chats"/><LoadingSpinner/>
       <chatBanner v-for="chat in chats" :chat="chat" lastMessage="franco"></chatBanner>
     </div>
 
-
-    <LoadingSpinner v-if="loading" loading="{{ loading }}" loadingText="Caricamento chats"/><LoadingSpinner/>
     <ErrorMsg v-if="errormsg" :msg="errormsg"></ErrorMsg>
   </div>
 </template>
 
 <style scoped>
-#chat_list_container{
+.chat-list-container{
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
 }
 
-.container{
+/* usato per quando devo mostare un messaggio centrato nel div della lista */
+.chat-list-show-message{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.nochat {
   position: relative;
-  height: 100%;
-  width: 100%;
+  width: fit-content;
+  background-color: #f0f0f0;
+  padding: 20px;
+  border-radius: 10px;
+  text-align: center;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  font-size: 18px;
+  color: #333;
 }
 </style>
