c76efaea76e6b3ea8fc34393c7b0c65ef7684275
Doct0rDuc <federicoiannucci2@gmail.com>
2025-03-13 22:55:09 +0100

    trying to fix yarn error, copypasted original webui folder

--- webui/vite.config.js
+++ webui/vite.config.js
@@ -12,11 +12,6 @@ export default defineConfig(({command, mode, ssrBuild}) => {
 				'@': fileURLToPath(new URL('./src', import.meta.url))
 			}
 		},
-		server: {
-			watch: {
-				usePolling: true,
-			}
-		},
 	};
 	ret.define = {
 		// Do not modify this constant, it is used in the evaluation.
