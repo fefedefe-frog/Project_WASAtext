36939dc89c470fdb7e8b49a484a5818caa661aa5
Doct0rDuc <federicoiannucci2@gmail.com>
2025-03-13 14:49:49 +0100

    finished the user list section, adjusted the loading bar and the message bar

--- webui/src/views/UsersView.vue
+++ webui/src/views/UsersView.vue
@@ -3,46 +3,72 @@ export default {
   data: function () {
     return {
       token: localStorage.getItem('authToken'),
+      usrId: localStorage.getItem('usrId'),
       errormsg: null,
       loading: false,
       users: [],
+      isOverlayVisible: false,
+      showMessage: false,
+      noUsers: false,
     }
   },
   methods: {
     async refresh() {
-      this.loading = true
-      this.errormsg = null
+      this.loading= true;
+      this.showMessage= true;
+
+      this.noUsers= false;
+      this.errormsg= null;
+
+      this.users= []
+
+      // Aggiungo dei delay grafici per rendere l'interfaccia più gradevole, in questo caso il delay
+      // aspetterà minimo 500ms prima di mostrare i risultati della richiesta al backend che verrà eseguita
+      // durante questo delay visivo
+      const delay = new Promise(resolve => setTimeout(resolve, 500));
+      const fetchUsers = this.$axios.get("/users", { headers: { Authorization: `${this.token}` } });
 
       try {
-        let response = await this.$axios.get("/users", {headers: {Authorization: `${this.token}`}});
-        this.users = []
+        let response= await Promise.all([fetchUsers, delay]).then(results => results[0]);
+
         if (response.data){
+          this.showMessage= false;
           response.data["users"].forEach(user => {
-            this.users.push(user)
+            if(user.usrId !== this.usrId){
+              this.users.push(user);
+            }
           })
+        }else{
+          this.users= [];
         }
       } catch (e) {
-        this.errormsg = e.toString();
+        if (e.response) {
+          if (e.response.status === 404) {
+            // In questo caso, se ho l'errore 404 significa solo che non ci sono utenti nel database
+            this.noUsers= true;
+
+          }else{
+            this.errormsg= e.toString();
+          }
+        }else{
+          this.errormsg= e.toString();
+        }
       } finally {
-        this.loading = false;
+        this.loading= false;
       }
     },
-    showOverlay() {
-      this.isOverlayVisible = true;
-    },
-    hideOverlay() {
-      this.isOverlayVisible = false;
-    },
-
+    toggleOverlay() {
+      this.isOverlayVisible = !this.isOverlayVisible;
+    }
   },
   mounted() {
-    this.refresh()
+    this.refresh();
   }
 }
 </script>
 
 <template>
-  <div>
+  <div class="container-main">
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
       <h1 class="h2">Users</h1>
       <div class="btn-toolbar mb-2 mb-md-0">
@@ -51,61 +77,78 @@ export default {
             Refresh
           </button>
           <div class="btn-group me-2">
-            <button type="button" class="btn btn-sm btn-outline-primary" @click="showOverlay">
+            <button type="button" class="btn btn-sm btn-outline-primary" @click="toggleOverlay">
               New Chat
             </button>
           </div>
         </div>
       </div>
     </div>
-    <div class="container">
-      <div id="users_list_container">
-        <UserBanner v-for="user in users" :user="user"></UserBanner>
 
-        <LoadingSpinner v-if="loading" loading="{{ loading }}" loadingText="Caricamento users"/><LoadingSpinner/>
+    <div class="container">
+      <div :class="showMessage ? 'user-list-show-message' : 'users-list-container'">
+        <div class="overlay" v-if="isOverlayVisible">
+          <!-- overlay per startare una nuova chat o vedere le info di un utente -->
+        </div>
+
+        <div v-if="showMessage && noUsers" class="nousers">
+          <p class="visually-visible centered">Non ci sono utenti nel sistema :(</p>
+        </div>
+
+        <LoadingSpinner v-if="loading" :loading="loading" loadingText="Caricamento users"/><LoadingSpinner/>
+        <UserBanner v-for="user in users" :user="user"></UserBanner>
       </div>
+
+      <ErrorMsg v-if="errormsg" :msg="errormsg"></ErrorMsg>
     </div>
-    <ErrorMsg v-if="errormsg" :msg="errormsg"></ErrorMsg>
   </div>
 
 </template>
 
 <style scoped>
-#users_list_container{
+.container-main {
+  height: 100%;
+  width: 100%;
+}
+
+.container {
+  width: 100%;
+  height: 85%;
+  overflow-y: scroll;
+  overflow-x: hidden;
+}
+.users-list-container{
+  position: relative;
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-  border: #0c4128 4px solid;
 }
 
-/* Contenitore principale */
-.container {
+/* usato per quando devo mostare un messaggio centrato nel div della lista */
+.user-list-show-message{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.nousers {
   position: relative;
-  width: 100%;
-  height: auto;
-  padding: 20px;
-  border: 4px solid darkorchid;
-}
-
-.overlay {
-  position: fixed; /* Cambia a "fixed" per far sì che stia sopra l'intera finestra */
-  top: 50%; /* Posizione centrata verticalmente */
-  left: 50%; /* Posizione centrata orizzontalmente */
-  transform: translate(-50%, -50%); /* Centra esattamente l'overlay */
-  background-color: rgba(0, 0, 0, 0.7); /* Sfondo semi-trasparente */
-  color: white;
-  padding: 20px;
-  border-radius: 10px;
-  border: #0a58ca 6px solid;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
-  z-index: 100; /* Assicurati che l'overlay stia sopra l'altro contenuto */
-}
-
-.modal {
-  background-color: #fff;
+  width: fit-content;
+  background-color: #f0f0f0;
   padding: 20px;
   border-radius: 10px;
   text-align: center;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  font-size: 18px;
   color: #333;
 }
 
+.overlay{
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(123, 174, 40, 0.5); /* Overlay semi-trasparente */
+  z-index: 1;
+}
 </style>
