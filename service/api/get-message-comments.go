3dc770ceceab16f7691aa5dbd70ddfb3e40b020d
Doct0rDuc <federicoiannucci2@gmail.com>
2025-04-06 22:46:28 +0200

    added participant check

--- service/api/get-message-comments.go
+++ service/api/get-message-comments.go
@@ -3,35 +3,49 @@ package api
 import (
 	"Project_WASAtext/service/api/reqcontext"
 	"Project_WASAtext/service/database"
-	"database/sql"
 	"encoding/json"
-	"errors"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
 	"strconv"
 )
 
-func (rt *_router) getMessageComments(writer http.ResponseWriter, _ *http.Request, params httprouter.Params, context reqcontext.RequestContext, _ string) {
+func (rt *_router) getMessageComments(writer http.ResponseWriter, _ *http.Request, params httprouter.Params, context reqcontext.RequestContext, token string) {
 
-	// Recupero l'id del messaggio dai paramentri dell'endpoint
-	msgId, err := strconv.Atoi(params.ByName("msg_id"))
+	// Recupero l'id della chat del messaggio dai paramentri dell'endpoint
+	chatId, err := strconv.Atoi(params.ByName("chat_id"))
 	if err != nil {
-		context.Logger.WithError(err).Error("invalid message id")
-		http.Error(writer, "invalid msg_id parameter", http.StatusBadRequest)
+		context.Logger.WithError(err).Error("invalid chat id")
+		http.Error(writer, "invalid chat_id parameter", http.StatusBadRequest)
+		return
+	}
+	var msgId int
+	msgId, err = strconv.Atoi(params.ByName("msg_id"))
+	if err != nil {
+		context.Logger.WithError(err).Error("invalid chat id")
+		http.Error(writer, "invalid chat_id parameter", http.StatusBadRequest)
+		return
+	}
+
+	// Controllo che l'utente che vuole commentare il messaggio faccia parte della chat di quel messaggio
+	var isParticipant bool
+	isParticipant, err = rt.db.CheckIfUserIsParticipant(chatId, token)
+	if err != nil {
+		context.Logger.WithError(err).Error("Error while checking the user participant status")
+		http.Error(writer, "Internal Server Error - Unable to check the user participant status", http.StatusInternalServerError)
+		return
+	}
+	if !isParticipant {
+		context.Logger.Warn("the user is not a participant of the chat of the message that was try to comment")
+		http.Error(writer, "Forbidden - Can't get the comments of a message from a chat where aren't participant of", http.StatusForbidden)
 		return
 	}
 
 	var messageComments []database.Comment
 	messageComments, err = rt.db.GetMessageComments(msgId)
 	if err != nil {
-		if errors.Is(err, database.ErrMessageHaveNoComments) {
-			http.Error(writer, "Not Found - message doesn't have any comment yet", http.StatusNotFound)
-			return
-		} else if errors.Is(err, sql.ErrNoRows) {
-			context.Logger.WithError(err).Warn("Error while checking the message comments")
-			http.Error(writer, "Not Found - message don't exist", http.StatusNotFound)
-			return
-		}
+		context.Logger.WithError(err).Warn("Error while checking the message comments")
+		http.Error(writer, "Internal server errror", http.StatusInternalServerError)
+		return
 	}
 
 	// Preparo la risposta contenente i commenti del messaggio
