c8a8d015dd7d1c7b8a47b3e6fa08024c69bfbfef
Doct0rDuc <federicoiannucci2@gmail.com>
2025-04-06 22:54:37 +0200

    added participant check

--- service/api/post-forward-message.go
+++ service/api/post-forward-message.go
@@ -6,13 +6,21 @@ import (
 	"encoding/json"
 	"errors"
 	"github.com/julienschmidt/httprouter"
+	"github.com/sirupsen/logrus"
 	"net/http"
 	"strconv"
 )
 
 func (rt *_router) forwardMessage(writer http.ResponseWriter, request *http.Request, params httprouter.Params, context reqcontext.RequestContext, token string) {
 
-	msgId, err := strconv.Atoi(params.ByName("msg_id"))
+	chatId, err := strconv.Atoi(params.ByName("chat_id"))
+	if err != nil {
+		context.Logger.WithError(err).Error("invalid message id parameter")
+		http.Error(writer, "invalid parameter", http.StatusBadRequest)
+		return
+	}
+	var msgId int
+	msgId, err = strconv.Atoi(params.ByName("msg_id"))
 	if err != nil {
 		context.Logger.WithError(err).Error("invalid message id parameter")
 		http.Error(writer, "invalid parameter", http.StatusBadRequest)
@@ -27,6 +35,33 @@ func (rt *_router) forwardMessage(writer http.ResponseWriter, request *http.Requ
 	if err := json.NewDecoder(request.Body).Decode(&requestJson); err != nil {
 		context.Logger.WithError(err).Error("Invalid JSON in requestBody")
 		http.Error(writer, "Invalid JSON format", http.StatusBadRequest)
+		return
+	}
+
+	// Controllo se l'utente che vuole inoltrare il messaggio fa parte della chat
+	// del messaggio da inoltrare, e della chat dove verrà inoltrato il messaggio
+	var isParticipant bool
+	isParticipant, err = rt.db.CheckIfUserIsParticipant(chatId, token)
+	if err != nil {
+		context.Logger.WithError(err).WithFields(logrus.Fields{"usrId": token, "groupId": chatId}).Errorf("Error while checking if the user is member of the group")
+		http.Error(writer, "Internal Server Error - can't check user paricipation of the group", http.StatusInternalServerError)
+		return
+	}
+	if !isParticipant {
+		context.Logger.WithFields(logrus.Fields{"usrId": token, "groupId": chatId}).Warn("user tried to forward a message of a chat which he isn't a member of")
+		http.Error(writer, "Forbidden - can't forward a message of a chat where you aren't part off", http.StatusForbidden)
+		return
+	}
+	isParticipant = false
+	isParticipant, err = rt.db.CheckIfUserIsParticipant(requestJson.ChatToForward, token)
+	if err != nil {
+		context.Logger.WithError(err).WithFields(logrus.Fields{"usrId": token, "groupId": requestJson.ChatToForward}).Errorf("Error while checking if the user is member of the group")
+		http.Error(writer, "Internal Server Error - can't check user paricipation of the group", http.StatusInternalServerError)
+		return
+	}
+	if !isParticipant {
+		context.Logger.WithFields(logrus.Fields{"usrId": token, "groupId": requestJson.ChatToForward}).Warn("user tried to forward a message to a chat which he isn't a member of")
+		http.Error(writer, "Forbidden - can't forward a message to a chat where you aren't part off", http.StatusForbidden)
 		return
 	}
 
