79c165b86050e0f3fc4eb69c31d57d43c166d5c0
Doct0rDuc <federicoiannucci2@gmail.com>
2025-04-06 22:55:58 +0200

    added participant check and response body

--- service/api/post-send-message.go
+++ service/api/post-send-message.go
@@ -3,10 +3,9 @@ package api
 import (
 	"Project_WASAtext/service/api/reqcontext"
 	"Project_WASAtext/service/database"
-	"database/sql"
 	"encoding/json"
-	"errors"
 	"github.com/julienschmidt/httprouter"
+	"github.com/sirupsen/logrus"
 	"net/http"
 	"strconv"
 )
@@ -26,15 +25,18 @@ func (rt *_router) sendMessage(writer http.ResponseWriter, request *http.Request
 		context.Logger.WithError(err).Error("invalid chat id")
 		http.Error(writer, "invalid chat_id parameter", http.StatusBadRequest)
 	}
+
 	// Controllo che l'utente faccia effettivamente parte del gruppo
-	if isParticipant, err := rt.db.CheckIfUserIsParticipant(chatId, token); !isParticipant {
-		if errors.Is(err, sql.ErrNoRows) {
-			context.Logger.Warnf("user <%s> tried to send a message to the chat <%d> which he isn't a member of", token, chatId)
-			http.Error(writer, "Forbidden - can't send a message of a chat which aren't member of", http.StatusForbidden)
-			return
-		}
-		context.Logger.WithError(err).Errorf("Error while checking if the user <%s> is member of the chat <%d>", token, chatId)
-		http.Error(writer, "Internal Server Error - can't check user paricipation of the chat", http.StatusInternalServerError)
+	var isParticipant bool
+	isParticipant, err = rt.db.CheckIfUserIsParticipant(chatId, token)
+	if err != nil {
+		context.Logger.WithError(err).WithFields(logrus.Fields{"usrId": token, "chatId": chatId}).Errorf("Error while checking if the user is member of the group")
+		http.Error(writer, "Internal Server Error - can't check user paricipation of the group", http.StatusInternalServerError)
+		return
+	}
+	if !isParticipant {
+		context.Logger.WithFields(logrus.Fields{"usrId": token, "groupId": chatId}).Warn("user tried send a message in a chat of which he isn't a member of")
+		http.Error(writer, "Forbidden - can't send a message in a chat where you aren't member off", http.StatusForbidden)
 		return
 	}
 
@@ -45,7 +47,7 @@ func (rt *_router) sendMessage(writer http.ResponseWriter, request *http.Request
 		return
 	}
 
-	context.Logger.Infof("user <%s> want to send a message to chat <%d>", token, chatId)
+	context.Logger.WithFields(logrus.Fields{"usrId": token, "chatId": chatId}).Info("user want to send a message in the chat")
 
 	// Aggiungo il messaggio al db
 	var newMessage database.Message
@@ -55,12 +57,26 @@ func (rt *_router) sendMessage(writer http.ResponseWriter, request *http.Request
 	newMessage.DeliveryStatus = "sent"
 	newMessage.IsForwarded = false
 
-	if err := rt.db.InsertMessage(newMessage, chatId); err != nil {
+	newMessage.MsgId, err = rt.db.InsertMessage(newMessage, chatId)
+	if err != nil {
 		context.Logger.WithError(err).Error("Error while inserting new message in the db")
 		http.Error(writer, "Internal Server Error - Unable to send the message", http.StatusInternalServerError)
 		return
 	}
 
-	// Invio la risposta senza corpo
-	writer.WriteHeader(http.StatusNoContent)
+	// Se tutti i controlli vanno a buon fine procedo a preparare la risposta e inviarla
+	responseJSON, marshalErr := json.Marshal(map[string]interface{}{"message": newMessage})
+	if marshalErr != nil {
+		context.Logger.WithError(marshalErr).Errorf("Failed to marshal the chat messages")
+		http.Error(writer, "Internal server error - failed json conversion", http.StatusInternalServerError)
+		return
+	}
+
+	// Scrivo la risposta
+	writer.Header().Set("Content-Type", "application/json")
+	if _, err := writer.Write(responseJSON); err != nil {
+		context.Logger.WithError(err).Error("Errore preaparazione risposta html")
+		http.Error(writer, "Internal server error", http.StatusInternalServerError)
+		return
+	}
 }
