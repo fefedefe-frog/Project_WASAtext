c66b2f097d1a05dd3c0f816a31d8467074f6aabc
Doct0rDuc <federicoiannucci2@gmail.com>
2025-05-08 21:23:36 +0200

    implemented new component UserInfo.vue

--- webui/src/main.js
+++ webui/src/main.js
@@ -10,6 +10,7 @@ import UserBanner from './components/UserBanner.vue'
 import ChatMessage from './components/ChatMessage.vue'
 import Chat from './components/Chat.vue'
 import UserInfo from './components/UserInfo.vue'
+import MessageForm from "./components/MessageForm.vue";
 
 import './assets/dashboard.css'
 import './assets/main.css'
@@ -24,6 +25,7 @@ app.component("UserBanner", UserBanner);
 app.component("ChatMessage", ChatMessage);
 app.component("Chat", Chat);
 app.component("UserInfo", UserInfo);
+app.component("MessageForm", MessageForm);
 
 app.use(router)
 app.mount('#app')
