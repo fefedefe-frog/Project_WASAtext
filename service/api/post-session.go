0c42dcd4d01125afe75b8da9144fbe0ec678231e
Doct0rDuc <federicoiannucci2@gmail.com>
2025-05-11 23:21:46 +0200

    updated function for responding with all the info of the user just logged

--- service/api/post-session.go
+++ service/api/post-session.go
@@ -2,11 +2,11 @@ package api
 
 import (
 	"Project_WASAtext/service/api/reqcontext"
+	"Project_WASAtext/service/database"
 	"Project_WASAtext/service/utilitytool"
 	"database/sql"
 	"encoding/json"
 	"errors"
-	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
 	"strings"
@@ -49,6 +49,7 @@ func (rt *_router) doLogin(writer http.ResponseWriter, request *http.Request, _ 
 	}
 
 	var usrId string
+	var user database.User
 	usrId, err = rt.db.GetUsrIdByName(usernameLower)
 
 	// Controllo se l'utente esiste ed è presente nel database se non è presente lo creo e provo ad inserirlo nel database
@@ -56,39 +57,35 @@ func (rt *_router) doLogin(writer http.ResponseWriter, request *http.Request, _ 
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			context.Logger.WithField("db error", err).Debug("utente non presente nel database")
-
-			context.Logger.Debug(fmt.Sprintf("Creazione e aggiunta del nuovo utente '%s' al database", usernameLower))
-			user, err := rt.db.InsertNewUser(requestJson.UserName)
-
+			context.Logger.WithField("username", usernameLower).Debug("Creazione e aggiunta del nuovo utente al database")
+			user, err = rt.db.InsertNewUser(requestJson.UserName)
 			if err != nil {
 				context.Logger.WithError(err).Error("Impossibile aggiungere nuovo user al database")
 				http.Error(writer, "Internal server error", http.StatusInternalServerError)
 				return
 			}
-			rt.sendJsonResponse(writer, user.UsrId, context)
-			return
 		} else {
 			context.Logger.WithError(err).Error("Errore durante il recupero dei dati")
 			http.Error(writer, "Internal server error", http.StatusInternalServerError)
 			return
 		}
+	} else {
+		user, err = rt.db.GetUserInfo(usrId)
+		if err != nil {
+			context.Logger.WithError(err).Error("unable to retrive user info")
+			http.Error(writer, "Internal server error - unable to retrive user info after login", http.StatusInternalServerError)
+			return
+		}
 	}
 
+	rt.sendJsonResponse(writer, user, context)
 	context.Logger.WithField("usrId", usrId).Info("login effettuato")
-	rt.sendJsonResponse(writer, usrId, context)
 }
 
-func (rt *_router) sendJsonResponse(writer http.ResponseWriter, usrId string, context reqcontext.RequestContext) {
+func (rt *_router) sendJsonResponse(writer http.ResponseWriter, user database.User, context reqcontext.RequestContext) {
 
-	// Creo la risposta http contentente il token di autorizzazione e l'usrId (in questo caso entrambi sono la stessa cosa
-	user, err := rt.db.GetUserInfo(usrId)
-	if err != nil {
-		context.Logger.WithError(err).Error("unable to retrive user info")
-		http.Error(writer, "Internal server error - unable to retrive user info after login", http.StatusInternalServerError)
-		return
-	}
-
-	responseJSON, marshalErr := json.Marshal(user)
+	// Creo la risposta http contentente il token di autorizzazione e l'user
+	responseJSON, marshalErr := json.Marshal(map[string]interface{}{"user": user})
 	if marshalErr != nil {
 		context.Logger.WithError(marshalErr).Errorf("Failed to marshal the user")
 		http.Error(writer, "Internal server error - failed json conversion", http.StatusInternalServerError)
@@ -97,7 +94,7 @@ func (rt *_router) sendJsonResponse(writer http.ResponseWriter, usrId string, co
 	}
 
 	// Scrivo nell'header della risposta il token bearer che in questo caso corrisponde all'usrId
-	writer.Header().Set("Authorization", "Bearer "+usrId)
+	writer.Header().Set("Authorization", "Bearer "+user.UsrId)
 
 	writer.Header().Set("Content-Type", "application/json")
 	if _, err := writer.Write(responseJSON); err != nil {
