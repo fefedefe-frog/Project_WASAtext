f8e59c1c318a1b0a6373fba45d4cffcc55b62825
Doct0rDuc <federicoiannucci2@gmail.com>
2025-05-21 03:27:58 +0200

    new users view, now it shows all the users of the app(with the SidebarList.vue component) and show the info of the user selected as children view

---
+++ webui/src/views/UsersView.vue
@@ -0,0 +1,71 @@
+<script>
+import {RouterView} from 'vue-router'
+
+export default {
+  components: {RouterView},
+  data: function () {
+    return {
+      token: '',
+      usrId: '',
+      errormsg: null,
+    }
+  },
+  mounted() {
+    this.token= sessionStorage.getItem('authToken');
+    this.usrId= sessionStorage.getItem('usrId');
+  },
+  methods: {
+    componentsErrorHandler(error){
+      this.errormsg= error;
+    },
+  }
+}
+</script>
+
+<template>
+  <div class="main-container">
+    <div class="lists bobby">
+      <SidebarList items="users" :bannerComponent="'UserBanner'" @error="componentsErrorHandler"/>
+    </div>
+    <div class="user-info-container bobby">
+      <RouterView />
+    </div>
+
+    <ErrorMsg v-if="errormsg" :msg="errormsg" />
+  </div>
+</template>
+
+<style scoped>
+.main-container {
+  height: 100%;
+  width: 100%;
+
+  display: flex;
+  flex-direction: row;
+
+  position: relative;
+}
+
+
+.lists {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  height: 100%;
+  width: 25%;
+  padding: 5px;
+
+  margin-right: 5px;
+}
+
+.user-info-container {
+  height: 100%;
+  width: 70%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+}
+</style>
