2039a89e91bb6ce260e444269c79e73658918e81
Doct0rDuc <federicoiannucci2@gmail.com>
2025-05-21 03:16:08 +0200

    modified App.vue, now it show one RouterView to the login view alone if not logged, or a navbar for moving between the views, and one RouterView

--- webui/src/App.vue
+++ webui/src/App.vue
@@ -1,10 +1,50 @@
 <script setup>
-import {RouterView} from 'vue-router'
+import {RouterLink, RouterView} from 'vue-router'
+</script>
+<script>
+export default {
+  methods: {
+    logout(){
+      sessionStorage.removeItem('authToken')
+      this.$router.push('/session')
+    }
+  }
+}
 </script>
 
 <template>
   <div class="app-div">
-    <main class="app-main">
+    <main v-if="$route.name !== 'login'" class="app-main">
+      <div class="navmenu bobby">
+        <div class="navmenu-submenu">
+          <RouterLink to="/users" class="navmenu-button">
+            <svg class="feather"><use href="/feather-sprite-v4.29.0.svg#users" /></svg>
+          </RouterLink>
+          <RouterLink to="/chats" class="navmenu-button">
+            <svg class="feather"><use href="/feather-sprite-v4.29.0.svg#message-circle" /></svg>
+          </RouterLink>
+          <RouterLink to="/newChat" class="navmenu-button">
+            <svg class="feather"><use href="/feather-sprite-v4.29.0.svg#plus-circle" /></svg>
+          </RouterLink>
+          <RouterLink to="/home" class="navmenu-button">
+            <svg class="feather"><use href="/feather-sprite-v4.29.0.svg#home" /></svg>
+          </RouterLink>
+        </div>
+        <div class="spacer"></div>
+        <div class="navmenu-submenu">
+          <RouterLink to="/" class="navmenu-button">
+            <svg class="feather"><use href="/feather-sprite-v4.29.0.svg#user" /></svg>
+          </RouterLink>
+          <button class="navmenu-button logout-button" @click="logout">
+            <svg class="feather"><use href="/feather-sprite-v4.29.0.svg#log-out" /></svg>
+          </button>
+        </div>
+      </div>
+      <div class="router-view">
+        <RouterView />
+      </div>
+    </main>
+    <main v-else>
       <RouterView />
     </main>
   </div>
@@ -14,6 +54,15 @@ import {RouterView} from 'vue-router'
 .app-div{
   width: 100vw;
   height: 100vh;
+
+  padding: 15px;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+
   /* schema di sfondo preso da uiverse */
   background-color: #313131;
   background-image: radial-gradient(rgba(255, 255, 255, 0.171) 2px, transparent 0);
@@ -28,6 +77,96 @@ import {RouterView} from 'vue-router'
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+.navmenu {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  height: 100%;
+  width: 5%;
+  padding: 2px;
+
+  margin-right: 5px;
+  overflow: hidden;
+  overflow-y: scroll;
+}
+
+.navmenu-submenu {
+  width: 100%;
+  height: fit-content;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.spacer {
+  width: 100%;
+  height: 60%;
+
+  border-top: 1px solid dimgray;
+  border-bottom: 1px solid dimgray;
+}
+
+.navmenu-button {
+  width: 40px;
+  height: 40px;
+
+  margin: 5px;
+
+  border-radius: 10px;
+  border: 2px dashed lightseagreen;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  text-decoration: none;
+  color: white;
+  background-color: lightseagreen;
+  cursor: pointer;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+  transition: .4s;
+
+}
+
+.logout-button {
+  background-color: red;
+  border: 2px dashed red;
+}
+
+.navmenu-button:hover {
+  transition: .4s;
+  border: 2px dashed lightseagreen;
+  background-color: white;
+  color: lightseagreen;
+}
+
+.logout-button:hover {
+  border: 2px dashed red;
+  color: red;
+}
+
+.navmenu-button:active {
+  background-color: lightseagreen;
+}
+
+.logout-button:active{
+  background-color: red;
+
+}
+
+.router-view {
+  height: 100%;
+  width: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
 }
 
 .bobby{
