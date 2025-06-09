f7a0c2625f6f63c48e87971a4f19e30f3e15eb96
Doct0rDuc <federicoiannucci2@gmail.com>
2025-06-09 02:28:56 +0200

    finished the view, now the user can create a group or a one2one chat with other

--- webui/src/views/NewChatView.vue
+++ webui/src/views/NewChatView.vue
@@ -4,27 +4,33 @@ export default {
     return {
       token: '',
       errormsg: null,
-      loading: false,
       users: [],
 
       chatInfo: {
         chatName: "",
-        chatPhoto: "",
+        chatPhoto: null,
+        chatPhotoPreview: "",
         isGroup: false,
         participantsId: [],
       },
+
 
       selectedParticipantInfo: [],
 
       initialMessage: {
         textContent: "",
         photoContent: null,
+        photoName: ""
       },
 
       setIntervalId: null,
     }
   },
   computed: {
+    chatNameIsValid() {
+      const name=  this.chatInfo['chatName'];
+      return (name.length >= 3 && name.length <= 16 && ((/^\S.*\S$/).test(name)));
+    },
     userFilteredResult(){
       let searchPool= this.users;
       let query= this.searchQuery.trim();
@@ -35,6 +41,11 @@ export default {
       return searchPool.filter(user =>
         user['userName'].toLowerCase().includes(this.searchQuery.toLowerCase())
       );
+    },
+    isFormSendable() {
+      let messageValid= this.initialMessage['textContent'].trim() || this.initialMessage['photoContent'];
+      let chatDataValid= this.chatNameIsValid && (this.chatInfo['participantsId'].length >= 1);
+      return messageValid && chatDataValid
     }
   },
   mounted() {
@@ -51,10 +62,12 @@ export default {
   },
   methods: {
     async getUsers() {
-      this.errormsg= null
+      this.errormsg= null;
 
       try {
-        let response= await this.$axios.get(`/users`, {headers: {Authorization: this.token}});
+        let response= await this.$axios.get(`/users`, {
+          headers: {Authorization: this.token}
+        });
 
         if (response.data) {
           if(response.data['users']){
@@ -70,23 +83,26 @@ export default {
         }else {
           this.errormsg = e;
         }
-      }finally {
-        this.loading = false
       }
     },
     async startNewChat(){
       this.errormsg= null;
 
+      if (this.chatInfo['participantsId'].length < 1){
+        this.errormsg= new Error("numero di partecipanti non valido");
+      }
+
       // Preparo il formData per la richiesta
       const requestFormData= new FormData();
 
-
       // Assegno le informazioni sulla chat
-      let chatName= ""
+      let chatName= "";
       let chatPhoto= new Blob([], {type: 'image/png'});
       if (this.chatInfo['isGroup']){
         chatName= this.chatInfo['chatName']
-        chatPhoto= this.chatInfo['chatPhoto']
+        if (this.chatInfo['chatPhoto']){
+          chatPhoto= this.chatInfo['chatPhoto']
+        }
       }
       requestFormData.append('chatName', chatName);
       requestFormData.append('chatPhoto', chatPhoto);
@@ -94,9 +110,19 @@ export default {
       requestFormData.append('participants', this.chatInfo['participantsId']);
 
       // Assegno le informazioni sul messaggio
-      requestFormData.append('messageTextContent', this.initialMessage['textContent']);
-      requestFormData.append('messagePhotoContent', this.initialMessage['photoContent']);
+      let textContent= this.initialMessage['textContent'];
+      let photoContent= new Blob([], {type: 'image/png'});
 
+      if (this.initialMessage['photoContent']){
+        textContent= "";
+        photoContent= this.initialMessage['photoContent'];
+      }
+
+      requestFormData.append('messageTextContent', textContent);
+      requestFormData.append('messagePhotoContent', photoContent);
+
+      console.log("form request:");
+      console.log(requestFormData);
       try{
         let response= await this.$axios.post(`/chats`, requestFormData, {
           headers: {
@@ -112,18 +138,60 @@ export default {
             chatPhoto: response.data['chatPhoto'],
             participants: response.data['participants']
           }
-          this.$router.push(`/chats/${newChat['chatId']}`);
+          setTimeout(this.$router.push(`/chats/${newChat['chatId']}`), 500);
         }
       }catch (e){
         this.errormsg= e;
       }
     },
+    imageUpload(target) {
+      const input= document.createElement('input');
+      input.type= "file";
+      input.accept= "image/*";
+
+      input.addEventListener("change", (event) => {
+
+        const file= event.target.files[0];
+        if (file && file.type.startsWith("image/")) {
+          const reader= new FileReader();
+
+          reader.onload= (e) => {
+            if (target === 'chatPhoto'){
+              this.chatInfo['chatPhoto']= file;
+              this.chatInfo['chatPhotoPreview']= e.target.result;
+            }else if (target === 'messagePhoto'){
+              this.initialMessage['textContent']= "";
+              this.initialMessage['photoContent']= file;
+              this.initialMessage['photoName']= file.name;
+            }
+          };
+          reader.readAsDataURL(file);
+        }else {
+          if (target === 'chatPhoto') {
+            this.chatInfo['chatPhoto'] = null;
+            this.chatInfo['chatPhotoPreview'] = "";
+          } else if (target === 'messagePhoto') {
+            this.initialMessage['photoContent'] = null;
+            this.initialMessage['photoName'] = "";
+          }
+        }
+
+      });
+      input.click();
+    },
     addParticipant(bannerData){
+      if(!this.chatInfo['isGroup']){
+        this.chatInfo['participantsId']= [];
+        this.selectedParticipantInfo= [];
+
+        this.chatInfo['chatPhotoPreview']= `data:image/png;base64,${bannerData['userPhoto']}`;
+        this.chatInfo['chatName']= bannerData['userName'];
+      }
 
       // Aggiungo l'utente alla lista di utenti, solo se non è già presente
       if (!this.chatInfo['participantsId'].some(usrId => usrId === bannerData['usrId'])){
         this.selectedParticipantInfo.push(bannerData);
-        this.chatInfo['participantsId'].push(bannerData['usrId'])
+        this.chatInfo['participantsId'].push(bannerData['usrId']);
       }
     },
     removeParticipant(id){
@@ -134,8 +202,19 @@ export default {
           pId !== id
       );
     },
+    clearFormData(){
+      this.chatInfo['chatName']= "";
+      this.chatInfo['chatPhoto']= null;
+      this.chatInfo['chatPhotoPreview']= "";
+      this.chatInfo['participantsId']= [];
+      this.selectedParticipantInfo= [];
+
+      this.initialMessage['textContent']= "";
+      this.initialMessage['photoContent']= null;
+      this.initialMessage['photoName']= "";
+    },
     componentsErrorHandler(error){
-      this.errormsg= error;
+      this.errormsg= error.toString();
     },
   }
 }
@@ -146,17 +225,31 @@ export default {
     <div class="select-participant">
       <sidebarList :bannerComponent="'userBanner'" items="users" @error="componentsErrorHandler" @bannerData="addParticipant"/>
     </div>
-    <div class="new-chat-form">
+    <form class="new-chat-form" @submit.prevent="startNewChat">
+      <!-- Sezione per le info della chat -->
       <div class="new-chat-info">
         <div class="new-chat-image">
-          immagine selezionabile, se cliccato
-          apre un menù per cambiare immagine
+          <button class="chat-image-button" type="button" @click="imageUpload('chatPhoto')" :disabled="!chatInfo['isGroup']">
+            <img v-if="chatInfo['chatPhotoPreview'] || chatInfo['isGroup']" :src=" chatInfo['chatPhotoPreview'] || '/images/def_group.png'" alt="Anteprima" draggable="false">
+            <span>
+              <svg class="feather"><use href="/feather-sprite-v4.29.0.svg#image" /></svg>
+            </span>
+          </button>
+          <input type="file" accept="/image/*" @change="" style="display: none">
         </div>
         <div class="new-chat-text">
           <div class="new-chat-name">
-            nome
+            <span>
+              <label for="chatName">Nome della chat:  <span v-if="!chatNameIsValid && chatInfo['chatName']" style="color: red; margin-top: 10px;">Nome non valido</span></label>
+              <input id="chatName" v-model="chatInfo['chatName']" type="text" :placeholder="chatInfo['isGroup'] ? 'Inserisci il nome' : ''" :disabled="!chatInfo['isGroup']" required>
+            </span>
+            <span>
+              <label for="isGroup">Nuovo gruppo</label>
+              <input id="isGroup" v-model="chatInfo['isGroup']" type="checkbox" @click="clearFormData">
+            </span>
           </div>
-          <div class="participants">
+          <!-- Sezione per la visualizzazione dei partecipanti di un gruppo -->
+          <div v-if="chatInfo['isGroup']" class="participants">
             <span v-for="p in selectedParticipantInfo" :key="p['usrId']" class="participant">
               <img :src="'data:image/png;base64,'+ p['userPhoto']" alt="Profile Image" draggable="false">
               <span>{{p['userName']}}</span>
@@ -167,12 +260,28 @@ export default {
           </div>
         </div>
       </div>
+      <!-- Sezione per il messaggio iniziale -->
       <div class="initial-message">
-        <div class="roberto">
-          <messageForm/>
+        <div class="mess-form">
+          <textarea v-if="!initialMessage['photoContent']" v-model="initialMessage['textContent']" class="textarea-content" placeholder="Scrivi un messaggio..." rows="2" :maxlength="1024" />
+          <div v-if="initialMessage['photoContent']" class="image-name">
+            <button class="form-buttons delete-button" type="button" @click="initialMessage['photoContent']=null; initialMessage['photoName']= ''">
+              <svg class="feather"><use href="/feather-sprite-v4.29.0.svg#x" /></svg>
+            </button>
+            <span>{{ initialMessage['photoName'] }}</span>
+          </div>
+          <div class="buttom-column">
+            <button class="form-buttons" type="button" @click="imageUpload('messagePhoto')">
+              <svg class="feather"><use href="/feather-sprite-v4.29.0.svg#image" /></svg>
+            </button>
+
+            <button class="form-buttons" type="submit" :disabled="!isFormSendable">
+              <svg class="feather"><use href="/feather-sprite-v4.29.0.svg#navigation" /></svg>
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </form>
     <ErrorMsg v-if="errormsg" :msg="errormsg" />
   </div>
 </template>
@@ -206,23 +315,6 @@ export default {
   border: 1px solid black;
 }
 
-.initial-message {
-  width: 75%;
-  height: 50%;
-
-  border: 1px solid darkgreen;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
-
-.roberto {
-
-  width: 50%;
-  height: 30%;
-}
-
 .new-chat-info {
   border: 1px solid red;
 
@@ -240,7 +332,62 @@ export default {
   height: 70%;
   aspect-ratio: 1/1;
   border: 1px solid blue;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
+
+.chat-image-button {
+  height: 90%;
+  aspect-ratio: 1/1;
+  border-radius: 50%;
+
+  padding: 0;
+  border: none;
+  background: lightgray;
+  position: relative;
+  overflow: hidden;
+}
+
+.chat-image-button img {
+  display: block;
+
+  width: 100%;
+  height: 100%;
+
+
+  object-fit: cover;
+  object-position: center;
+  pointer-events: none;
+}
+
+.chat-image-button:not(:disabled):hover {
+  filter: brightness(0.6);
+  transition: filter 0.2s ease-in-out;
+}
+
+.chat-image-button span {
+  position: absolute;
+  width: 40%;
+  height: 40%;
+
+  border-radius: 50%;
+  padding: 10px;
+
+  top: 50%;
+  left:50%;
+  transform: translate(-50%, -50%);
+
+  color: white;
+  background: rgba(0, 0, 0, 0.5);
+}
+
+.chat-image-button svg {
+  width: 100%;
+  height: 100%;
+}
+
 
 .new-chat-text {
   height: 70%;
@@ -255,11 +402,20 @@ export default {
 }
 
 .new-chat-name {
-  width: 45%;
+  width: fit-content;
+  max-width: 80%;
   height: 60%;
   border: 1px solid cyan;
 
+  display: flex;
+  flex-direction: column;
+
 }
+
+#chatName, #isGroup {
+  margin-left: 5px;
+}
+
 
 .participants {
   width: 100%;
@@ -341,5 +497,117 @@ export default {
 
 .participant button:hover {
   background: rgba(0, 0, 0, 0.5);
+}
+
+/* message form */
+.initial-message {
+  width: 75%;
+  height: 50%;
+
+  border: 1px solid darkgreen;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.mess-form {
+  width: 50%;
+  height: 30%;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: right;
+}
+
+.textarea-content {
+  width: 100%;
+  max-height: 100px;
+  margin: 2px;
+
+  border-radius: 10px;
+  padding: 2px 4px 0 4px;
+
+  resize: none;
+  overflow-x: hidden;
+  font-size: 0.8rem;
+  line-height: 1rem;
+  box-sizing: border-box;
+  border: 1px solid white;
+}
+
+.buttom-column {
+  width: fit-content;
+  height: fit-content;
+  margin-top: auto;
+  margin-left: 0.3vh;
+  display: flex;
+  flex-direction: column;
+}
+
+.form-buttons {
+  width: 25px;
+  height: 25px;
+
+  margin: 1px 5px 2px 0;
+
+  border-radius: 25%;
+  border: 2px dashed lightseagreen;
+
+
+  color: white;
+  background-color: lightseagreen;
+  cursor: pointer;
+
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+  transition: .4s;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.form-buttons:not(:disabled):hover {
+  transition: .4s;
+  border: 2px dashed lightseagreen;
+  background-color: white;
+  color: lightseagreen;
+}
+
+.form-buttons:active {
+  background-color: lightseagreen;
+}
+
+.form-buttons:disabled{
+  background-color: gray;
+  border: 2px solid gray;
+  cursor: default;
+}
+
+.delete-button {
+  border: 2px dashed red;
+  background: red;
+}
+.delete-button:hover {
+  border: 2px dashed darkred;
+  background-color: white;
+  color: red;
+}
+.delete-button:active {
+  border: 2px dashed darkred;
+}
+
+.image-name {
+  height: fit-content;
+  object-fit: contain;
+
+  margin-right: 2px;
+
+  display: flex;
+  flex-direction: row;
+
+  align-items: center;
+  justify-content: center;
+
 }
 </style>
