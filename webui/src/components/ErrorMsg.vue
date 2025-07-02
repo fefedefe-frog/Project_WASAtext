e4180cec71a05b2f0ec90d5600159a0911f0b827
Doct0rDuc <federicoiannucci2@gmail.com>
2025-07-02 03:58:10 +0200

    updated error popup, added close button

--- webui/src/components/ErrorMsg.vue
+++ webui/src/components/ErrorMsg.vue
@@ -5,21 +5,41 @@ export default {
       type: String,
       required: true
     }
-  }
+  },
+  emits: ['close'],
 }
 </script>
 
 <template>
   <div class="alert alert-danger error-div" role="alert">
-    {{ msg }}
+    <span>{{ msg }}</span>
+    <button class="close-button" @click="this.$emit('close')">Close</button>
   </div>
 </template>
 
 <style scoped>
 .error-div {
   position: absolute;
+
+  top: 2%;
   left: 50%;
+
   transform: translate(-50%);
   user-select: none;
+
+  display: flex;
+  flex-direction: row;
+
+  align-items: center;
+}
+
+.close-button{
+  width: 50px;
+  height: 30px;
+
+  margin-left: 10px;
+
+  border: 1px solid red;
+  border-radius: 5px;
 }
 </style>
