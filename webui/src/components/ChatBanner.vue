ba9518e8c264a6a92e4bfb9530c8ac6fab07c3e4
Doct0rDuc <federicoiannucci2@gmail.com>
2025-09-06 04:51:47 +0200

    removed the async function, changed style, added timestamp of the message

--- webui/src/components/ChatBanner.vue
+++ webui/src/components/ChatBanner.vue
@@ -7,72 +7,41 @@ export default {
     },
   },
   emits: [
-    'error', 'bannerClicked'
+    'error', 'bannerClicked',
   ],
   data() {
     return {
       token: "",
       usrId: "",
 
-      chat: {
-        chatId: -1,
-        chatName: "",
-        chatPhoto: "",
-        isGroup: false,
-        participants: []
-      },
+      chat: {},
       participantNames: {},
 
-      lastMessage: {
-        msgId: -1,
-        senderId: "",
-        respondTo: -1,
-        textContent: "",
-        photoContent: "",
-        timestamp: "",
-        comments: [],
-      },
+      lastMessage: {},
       lastMessPreview: "",
-      lastMsgId: -1
+      lastMessTimestamp: ""
     }
   },
   created() {
-    this.chat= this.inputData;
-    this.token= sessionStorage.getItem('authToken');
-    this.usrId= sessionStorage.getItem('usrId')
+    this.chat= this.inputData['chat'];
+    this.lastMessage= this.inputData['lastMsg'];
 
     this.chat['participants'].forEach(p =>{
       this.participantNames[p['usrId']]= p['userName'];
     });
 
   },
-  async mounted(){
-    await this.getChatMessages();
+  mounted(){
+    this.chat= this.inputData['chat'];
+    this.lastMessage= this.inputData['lastMsg'];
+
+    this.chat['participants'].forEach(p =>{
+      this.participantNames[p['usrId']]= p['userName'];
+    });
+
     this.makeMessPreview();
   },
   methods: {
-    async getChatMessages(){
-      try {
-        let response= await this.$axios.put(`/chats/${this.chat['chatId']}/messages`, {
-          msgId: this.lastMsgId
-        }, {
-          headers: {Authorization: this.token},
-        });
-
-        if (response.data) {
-          if (Array.isArray(response.data['messages']) && response.data['messages'].length > 0){
-            this.messages= [];
-            response.data['messages'].forEach(message => {
-              this.messages.push(message);
-            })
-            this.lastMessage= this.messages[this.messages.length-1];
-            this.lastMsgId= this.lastMessage['msgId'];
-          }
-        }
-      }catch(e) {
-        this.$emit('error', e);
-      }
-    },
     bannerClicked(){
       this.$emit('bannerClicked', this.chat['chatId'])
     },
@@ -88,6 +57,29 @@ export default {
         preview= `${preview} ${this.lastMessage['textContent']}`;
       }
       this.lastMessPreview= preview;
+
+      //Timestamp
+      if (this.lastMessage['timestamp']){
+        const dateObject= new Date(this.lastMessage['timestamp']);
+        const currDate= new Date();
+
+        // Formatto giorno mese anno dal timestamp
+        let dateFormatter = new Intl.DateTimeFormat('it-IT', { dateStyle: 'short' });
+        let formattedDate = dateFormatter.format(dateObject);
+
+        // Formatto ore e minuti
+        let timeFormatter = new Intl.DateTimeFormat('it-IT', { hour: '2-digit', minute: '2-digit' });
+        let formattedTime = timeFormatter.format(dateObject);
+
+        // calcolo la differenza tra le date, se maggiore di un giorno mostro il giorno del messaggio, se minore mostro la data
+        let giornoInMs= 24 * 60 * 60 * 1000;
+
+        if ((currDate.getTime() - dateObject.getTime()) > giornoInMs ){
+          this.lastMessTimestamp= formattedDate;
+        }else {
+          this.lastMessTimestamp= formattedTime;
+        }
+      }
     }
   },
 };
@@ -106,6 +98,7 @@ export default {
       <div class="message-preview">
         <span class="last-message-text">{{ lastMessPreview }}</span>
         <svg v-if="lastMessage['photoContent'].length > 0" class="feather"><use href="/feather-sprite-v4.29.0.svg#image" /></svg>
+        <span class="last-message-timestamp">{{ lastMessTimestamp }}</span>
       </div>
     </div>
   </div>
@@ -114,16 +107,19 @@ export default {
 <style scoped>
 
 .chat-banner {
+  height: 50px;
+  width: 100%;
+
+  padding: 5px;
+  margin-bottom: 5px;
+
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 5px;
-  margin-bottom: 5px;
+
   background-color: lightgray;
   border-radius: 8px;
   box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-  height: 70px;
-  width: 100%;
 }
 
 .chat-banner:hover {
@@ -182,21 +178,30 @@ export default {
 }
 
 .last-message-text {
-  user-select: none;
   height: fit-content;
   width: fit-content;
   max-width: 90%;
-
-
   margin: 0 2px 0 10px;
 
   color: dimgray;
   font-size: 0.9em;
   font-weight: normal;
+  user-select: none;
 
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
 }
 
+.last-message-timestamp{
+  height: fit-content;
+  width: fit-content;
+  margin-right: 2%;
+
+  font-style: oblique;
+  margin-left: auto;
+  font-size: 0.9em;
+
+  user-select: none;
+}
 </style>
